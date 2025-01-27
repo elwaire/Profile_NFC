e38e1fb03c36d662af0a950cf3385b7cbe22857f
min <minull1810@gmail.com>
2025-01-27 10:56:21 +0700

    updated 27/02/2025

--- src/core/store/reducers/resources/index.ts
+++ src/core/store/reducers/resources/index.ts
@@ -1,5 +1,5 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { Resource, ResourcesState } from './types';
+import { ResourcesState } from './types';
 
 
 const initialState: ResourcesState = {
